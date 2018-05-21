e926f42155fae49a9d3b762b852927bf6bde929e
Athina <ath.ctam@gmail.com>
2018-05-21 18:59:53 +0300

    emp

---
+++ htdocs/models/employee.php
@@ -0,0 +1,29 @@
+<?php 
+
+include 'mysql_connector.php';
+
+class Employee {
+
+    function __construct($employee_data) {
+        $this->irs_num = $employee_data[0];
+        $this->ssn = $employee_data[1];
+        $this->first_name = $employee_data[2];
+        $this->last_name = $employee_data[3];
+        //$this->stars = $hotel_data[4];
+    }
+
+    private static function createEmployee($employee_data) {
+        return new Employee($employee_data);
+    }
+
+    static function fetchAll() {
+        global $con;
+        $result = $con->query("SELECT * FROM EMPLOYEES");
+        $con->close();
+        $employee_data = $result->fetch_all();
+        return array_map(array('Employee','createEmployee'), $employee_data);
+    }
+    
+}
+
+?>
--- htdocs/views/employees.php
+++ htdocs/views/employees.php
@@ -1,4 +1,31 @@
-<?php
+<?php 
     $title = "Employees";
-    include 'templates/title.php'
+    
+    include 'templates/title.php';
+    include 'models/employee.php';
 ?>
+
+<?php
+    $employees = Employee::fetchAll();
+?>
+
+<table class="ui celled table">
+  <thead>
+    <tr><th>IRS Number</th>
+    <th>SSN</th>
+    <th>First Name</th>
+    <th>Last Name</th>
+  </tr></thead>
+  <tbody>
+  <?php 
+    foreach ($employees as $employee) { 
+  ?>
+    <tr>
+      <td><?php echo($employee->irs_num) ?></td>
+      <td><?php echo($employee->ssn) ?></td>
+      <td><?php echo($employee->first_name) ?></td>
+      <td><?php echo($employee->last_name) ?></td>
+       </tr>
+  <?php } ?>
+  </tbody>
+</table>
